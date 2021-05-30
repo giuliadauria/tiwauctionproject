33c6141c19fd45040b4df659661b9474d2bfb64e
giuliadauria <giulia.dauria@mail.polimi.it>
2021-05-30 16:20:23 +0200

    First commit test

---
+++ TIW-AuctionProject/src/main/java/it/polimi/tiw/auction/dao/PictureDAO.java
@@ -0,0 +1,47 @@
+package it.polimi.tiw.projects.dao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import it.polimi.tiw.projects.beans.Picture;
+
+public class PictureDAO {
+
+	Connection connection;
+	
+	public PictureDAO(Connection connection) {
+		this.connection = connection;
+	}
+	
+	public List<Picture> findPicturesByItemId(int itemId){
+		List<Picture> picturesList = new ArrayList<>();
+		String query = "SELECT id FROM image GROUP BY ?";
+		try(PreparedStatement pstatement = connection.prepareStatement(query)){
+			pstatement.setInt(1, itemId);
+			try(ResultSet result = pstatement.executeQuery()){
+				while(result.next()) {
+					Picture picture = new Picture();
+					picture.setItemId(result.getInt("itemId"));
+					picture.setPictureUrl(result.getString("url"));
+					picturesList.add(picture);
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		//if there are no entries corresponding to the query, null is an easier value to return and check
+		if(picturesList.size() == 0) {
+			return null;
+		}
+		else {
+			return picturesList;
+		}
+	}
+	
+	public void createPicture() {
+		
+	}
+	
+}
